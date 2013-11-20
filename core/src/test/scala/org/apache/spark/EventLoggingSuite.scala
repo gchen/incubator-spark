9e5df5b497972dc81a3acd69309a8d50e6ed89cb
Lian, Cheng <rhythm.mail@gmail.com>
2013-11-20 18:59:18 +0800

    Added EventLoggingSuite and the RDD creation test case

--- core/src/test/scala/org/apache/spark/EventLoggingSuite.scala
+++ core/src/test/scala/org/apache/spark/EventLoggingSuite.scala
@@ -1,12 +1,31 @@
 package org.apache.spark
 
-/**
- * Created with IntelliJ IDEA.
- * User: lian
- * Date: 11/20/13
- * Time: 5:26 PM
- * To change this template use File | Settings | File Templates.
- */
-class EventLoggingSuite {
+import org.scalatest.FunSuite
+import org.apache.spark.scheduler.{SparkListenerRDDCreation, SparkListener}
+import akka.dispatch.{Await, Promise}
+import akka.util.duration._
+import org.apache.spark.rdd.EmptyRDD
 
+class EventLoggingSuite extends FunSuite with LocalSparkContext {
+  import LocalSparkContext.withSpark
+
+  implicit val actorSystem = sc.env.actorSystem
+
+  def withLocalSpark[T](f: SparkContext => T) =
+    withSpark(new SparkContext("local", "test"))(f)
+
+  test("A SparkListenerRDDCreation event should be posted when an RDD is created") {
+    withLocalSpark { sc =>
+      val eventPosted = Promise[Boolean]()
+
+      sc.addSparkListener(new SparkListener {
+        override def onRDDCreation(rddCreation: SparkListenerRDDCreation) {
+          eventPosted.success(true)
+        }
+      })
+
+      new EmptyRDD[Unit](sc)
+      assert(Await.result(eventPosted.future, 1 second) === true)
+    }
+  }
 }
