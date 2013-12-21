d7fc4ff3abaa1138dd1b2a94fc75dc2f67b2d6c3
Lian, Cheng <rhythm.mail@gmail.com>
2013-12-22 01:12:35 +0800

    Fixed constructor parameter of event log I/O streams

--- core/src/main/scala/org/apache/spark/EventLogStreams.scala
+++ core/src/main/scala/org/apache/spark/EventLogStreams.scala
@@ -3,11 +3,11 @@ package org.apache.spark
 import java.io._
 
 class EventLogInputStream(stream: InputStream, val context: SparkContext)
-  extends ObjectInputStream {
+  extends ObjectInputStream(stream) {
 
   override def resolveClass(desc: ObjectStreamClass) =
     Class.forName(desc.getName, false, Thread.currentThread().getContextClassLoader)
 }
 
 class EventLogOutputStream(stream: OutputStream)
-  extends ObjectOutputStream
+  extends ObjectOutputStream(stream)
